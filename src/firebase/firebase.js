89cbfa9ff606be6b9a87ad3899236d749d11ed97
ultofweb <mehmetalibuber6@gmail.com>
2023-02-06 14:12:25 +0300

    firebase integration

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+//import Firebase from "firebase";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe8Jgxh2_mkK31tIZMi-JYlbyc9Yu5OhU",
+  authDomain: "todo-ultof-com.firebaseapp.com",
+  projectId: "todo-ultof-com",
+  storageBucket: "todo-ultof-com.appspot.com",
+  messagingSenderId: "831728805969",
+  appId: "1:831728805969:web:1987a533530f895d993070"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
